fccc48a3ad8f9c78744b70ce91104755c71747a3
Promish Gurung <promishgrg98@gmail.com>
2024-05-17 10:27:02 +0545

    Add Navbar menu item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,8 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
 
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <div>
         <nav className='navbar'>
@@ -17,6 +19,20 @@ function Navbar() {
             <div className='menu-icon' onClick={handleClick}>
               <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu} >Home</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/services' className='nav-links' onClick={closeMobileMenu} >Services</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/products' className='nav-links' onClick={closeMobileMenu} >Products</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu} >Sign Up</Link>
+              </li>
+            </ul>
           </div>
         </nav>
     </div>
