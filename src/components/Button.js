ea87c62000995609cad0ee153e577dee67ac29f3
Promish Gurung <promishgrg98@gmail.com>
2024-05-18 18:11:45 +0545

    Add Navbar css

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,8 @@ const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    const checkButtonStyle = STYLES.include(buttonStyle) ? buttonStyle : STYLES[0];
-    const checkButtonSize = SIZES.include(buttonSize) ? buttonSize : SIZES[0];
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
         <Link to='/sign-up' className='btn-mobile'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
 
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
 
   return (
     <div>
@@ -33,6 +45,7 @@ function Navbar() {
                 <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu} >Sign Up</Link>
               </li>
             </ul>
+            {button && <Button buttonStyle={'btn--outline'}>Sign Up</Button>}
           </div>
         </nav>
     </div>
